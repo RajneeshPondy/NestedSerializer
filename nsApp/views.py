a9bf54fbd8c325652cdce595697affc07b916c7d
RajneeshPython <rajneeshkmr668@gmail.com>
2020-12-27 18:07:35 +0530

    Class level Authentication and Permissions

--- nsApp/views.py
+++ nsApp/views.py
@@ -2,11 +2,15 @@ from rest_framework import generics
 
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
+from rest_framework.authentication import BasicAuthentication
 
 
 class AuthorListAPIView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 
 class AuthorDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
